254b308d43ecfa00c4f7ff7d56c653394d524a7d
vlad <kozii.vladyslav@gmail.com>
2017-10-05 00:32:01 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## How to build?
+
+1. Install [gradle](https://gradle.org/)
+   
+   - *run command*
+   ```commandline
+    gradle build
+    ```
---
+++ src/main/kotlin/ua/kpi/atlantida/App.kt
@@ -0,0 +1,9 @@
+package ua.kpi.atlantida
+
+/**
+ * Created by vlad on 05.10.17.
+ */
+
+fun main(args: Array<String>) {
+    println("Hello Atlantida!!!")
+}
